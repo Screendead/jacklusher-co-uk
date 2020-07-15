866026588b013464195a2405a1f2bf1edcc339e0
Jack Lusher <jacklusher@jacks-mbp.home>
2020-07-15 11:44:15 +0100

    Create visual navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="navbar">
+      <div className="slider">
+        <NavSection grow={0} rotate>
+          <NavLink icon={['fas', 'ellipsis-h']} />
+        </NavSection>
+        <NavSection grow={1}>
+          <NavLink icon={['fas', 'user-circle']} anchor="javascript:alert('user');" />
+          <NavLink icon={['fas', 'cogs']} anchor="javascript:alert('settings');" />
+        </NavSection>
+      </div>
+    </nav>
+  );
+}
+
+function NavSection(props) {
+  return (
+    <div className={"nav-section" + (props.rotate ? " rotate" : "")} style={{ flexGrow: props.grow }}>{props.children}</div>
+  );
+}
+
+function NavLink(props) {
+  if (!props.anchor) {
+    return (
+      <span className="nav-link-container">
+        <NavIcon name={props.icon} />
+      </span>
+    )
+  }
+
+  return (
+    <span className="nav-link-container">
+      <a className="nav-link" href={props.anchor}>
+        <NavIcon name={props.icon} />
+      </a>
+    </span>
+  )
+}
+
+function NavIcon(props) {
+  return (
+    <span className="nav-icon">
+      <FontAwesomeIcon icon={props.name} />
+    </span>
   );
 }
 
